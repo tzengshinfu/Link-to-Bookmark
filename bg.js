aa732725503e67bd9fcc4fcdeecae068af64b746
tzengshinfu <tzengshinfu@gmail.com>
2015-01-21 00:29:52 +0800

    send selectedLinks to bookmark using chrome.runtime.connect.

--- bg.js
+++ bg.js
@@ -18,12 +18,17 @@ function addBookmark(info) {
 }
 
 
+function addBookmarks(info) {
+}
+
+
 onInitial();
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
     linkText = request.linkText;
 });
 chrome.contextMenus.create({title: "Add this link to bookmark", contexts:["link"], onclick: addBookmark});
+chrome.contextMenus.create({title: "Add this link[s] to bookmark", contexts:["selection"], onclick: addBookmarks});
 chrome.tabs.onActivated.addListener(function(info) {
     chrome.tabs.executeScript(null, {file: "contentscript.js"});
 });
--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,20 @@
 document.addEventListener('contextmenu', function(evt) {
+    getLinkText(evt);
+    getSelectionLinks();
+}, false);
+
+
+var allLinks = document.getElementsByTagName("a");
+var allLinksAmount = allLinks.length;
+
+
+function getSelectionLinks() {
+    
+    document.getSelection().containsNode(allLinks[10], false);
+}
+
+
+function getLinkText(evt) {
     var target = evt.target || evt.srcElement, linkText = target.textContent || target.innerText || target.alt || target.src;
     chrome.runtime.sendMessage({linkText: linkText});
-}, false);
+}
